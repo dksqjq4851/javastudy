043a4a8bf257fcf3de04892849c304ef3a7dd8ee
dksqjq4851 <dksqjq4851@naver.com>
2021-03-25 17:48:28 +0900

    update 2021-03-25

---
+++ workspace/11_jdbc/src/main/StaffMainClass.java
@@ -0,0 +1,12 @@
+package main;
+
+public class StaffMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaffManager stm = new StaffManager();
+		stm.execute();
+
+	}
+
+}
